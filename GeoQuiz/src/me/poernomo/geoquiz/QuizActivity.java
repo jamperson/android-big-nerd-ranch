965349fb65dbf8260d6130423eea8317cf2a5362
jefferpoe <jefferson@poernomo.me>
2013-10-19 00:09:40 +1100

    end of chapter 3: landscape layout saveInstanceState logging

--- GeoQuiz/src/me/poernomo/geoquiz/QuizActivity.java
+++ GeoQuiz/src/me/poernomo/geoquiz/QuizActivity.java
@@ -2,18 +2,22 @@ package me.poernomo.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class QuizActivity extends Activity
-{
+public class QuizActivity extends Activity {
 
-	private Button mTrueButton;
-	private Button mFalseButton;
-	private Button mNextButton;
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
+	private Button mTrueButton, mFalseButton;
+	private ImageButton mNextButton, mPreviousButton;
+
 	private TextView mQuestionTextView;
 
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -27,6 +31,7 @@ public class QuizActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
@@ -49,21 +54,59 @@ public class QuizActivity extends Activity
 			}
 		});
 
-		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton = (ImageButton) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v)
 			{
 
-				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-				updateQuestion();
+				getNextQuestion();
+			}
+		});
+
+		mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+		mPreviousButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v)
+			{
+
+				getPrevQuestion();
 			}
 		});
 
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v)
+			{
+				getNextQuestion();
+
+			}
+		});
+		
+		if( savedInstanceState != null)
+		{
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+		}
 		updateQuestion();
 
+	}
+
+	private void getPrevQuestion()
+	{
+		mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+		if (mCurrentIndex < 0)
+			mCurrentIndex += mQuestionBank.length;
+		updateQuestion();
+	}
+
+	private void getNextQuestion()
+	{
+		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+		updateQuestion();
 	}
 
 	private void updateQuestion()
@@ -83,6 +126,54 @@ public class QuizActivity extends Activity
 
 		Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT)
 				.show();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "onSaveInstanceState");
+		outState.putInt(KEY_INDEX, mCurrentIndex);
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+	@Override
+	protected void onPause()
+	{
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onResume()
+	{
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+	@Override
+	protected void onStart()
+	{
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	protected void onStop()
+	{
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d(TAG, "onStop() called");
 	}
 
 	@Override
