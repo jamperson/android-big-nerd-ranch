d6d86eeb981376c4c0629a600d16323a9c7db386
jefferpoe <jefferson@poernomo.me>
2013-10-30 23:31:45 +1100

    fully transitioned to using ViewPager and FragmentStatePagerAdapter, removed CrimeActivity. Using CrimePagerActivity instead now
    Initialized CrimePagerActivity properly to always start with the
    selected Crime ID
    
    implemented setOnPageChangedListener to update the activity's title
    whenever CrimeFragment is changed
    
    fixed minor bug: not updating the activity's title on first load

--- CriminalIntent/src/me/poernomo/android/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/src/me/poernomo/android/criminalintent/CrimePagerActivity.java
@@ -1,6 +1,7 @@
 package me.poernomo.android.criminalintent;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,7 +14,7 @@ public class CrimePagerActivity extends FragmentActivity {
 
 	private ViewPager mViewPager;
 	private ArrayList<Crime> mCrimes;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -21,9 +22,9 @@ public class CrimePagerActivity extends FragmentActivity {
 		mViewPager = new ViewPager(this);
 		mViewPager.setId(R.id.viewPager);
 		setContentView(mViewPager);
-		
+
 		mCrimes = CrimeLab.get(this).getCrimes();
-		
+
 		FragmentManager fm = getSupportFragmentManager();
 		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
 
@@ -39,11 +40,53 @@ public class CrimePagerActivity extends FragmentActivity {
 			{
 				return mCrimes.size();
 			}
-			
+
 		});
-		
+
+		// Which Crime should we display when we first load up?
+		UUID crimeId = (UUID) getIntent().getSerializableExtra(
+				CrimeFragment.EXTRA_CRIME_ID);
+		Crime tempCrime;
+		for (int i = 0; i < mCrimes.size(); i++)
+		{
+			tempCrime = mCrimes.get(i);
+			if (tempCrime.getId().equals(crimeId))
+			{
+				mViewPager.setCurrentItem(i);
+				setTitle(tempCrime.getTitle()); // fixed minor bug: not setting
+												// activity title for first load
+				break;
+			}
+		}
+
+		mViewPager
+				.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+					@Override
+					public void onPageSelected(int arg0)
+					{
+						Crime crime = mCrimes.get(arg0);
+						if (crime.getTitle() != null)
+						{
+							setTitle(crime.getTitle());
+						}
+					}
+
+					@Override
+					public void onPageScrolled(int arg0, float arg1, int arg2)
+					{
+						// intentionally blank
+
+					}
+
+					@Override
+					public void onPageScrollStateChanged(int arg0)
+					{
+						// intentionally blank
+
+					}
+				});
+
 	}
 
-	
-	
 }
