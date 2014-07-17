21e784be9cf3df8d4fa45fdc57ecaf5a1971c360
jefferson.poe <jefferson.poe@DWQA3-JPOE.office.globalrelay.net>
2014-07-16 17:14:20 -0700

    still on page 103... on to "Changing activated item backgrounds"

--- CriminalIntent/src/me/poernomo/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/me/poernomo/android/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView.MultiChoiceModeListener;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -64,6 +66,54 @@ public class CrimeListFragment extends ListFragment {
 		} else {
 			// use contextual action bar on honeycomb and higher
 			listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+			listView.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+
+				@Override
+				public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				@Override
+				public void onDestroyActionMode(ActionMode mode) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+					MenuInflater inflater = mode.getMenuInflater();
+					inflater.inflate(R.menu.crime_list_item_context, menu);
+					return true;
+				}
+
+				@Override
+				public boolean onActionItemClicked(ActionMode mode,
+						MenuItem item) {
+					switch (item.getItemId()) {
+					case R.id.menu_item_delete_crime:
+						CrimeAdapter adapter = (CrimeAdapter) getListAdapter();
+						CrimeLab crimeLab = CrimeLab.get(getActivity());
+						for (int i = adapter.getCount() - 1; i >= 0; i--) {
+							if (getListView().isItemChecked(i)) {
+								crimeLab.deleteCrime(adapter.getItem(i));
+							}
+						}
+						mode.finish();
+						adapter.notifyDataSetChanged();
+						return true;
+					default:
+						return false;
+					}
+				}
+
+				@Override
+				public void onItemCheckedStateChanged(ActionMode mode,
+						int position, long id, boolean checked) {
+					// TODO Auto-generated method stub
+
+				}
+			});
 		}
 		return v;
 	}
--- CriminalIntent/src/me/poernomo/android/criminalintent/MyDateFormat.java
+++ CriminalIntent/src/me/poernomo/android/criminalintent/MyDateFormat.java
@@ -3,6 +3,6 @@ package me.poernomo.android.criminalintent;
 public class MyDateFormat {
 	// Simple date format
 	public static final CharSequence SHORT_DATE = "EEEE, d MMM, yyy";
-	public static final CharSequence SHORT_TIME = "h:m a";
-	public static final CharSequence SHORT_DATE_TIME = "EEEE, d MMM, yyy - h:m a";
+	public static final CharSequence SHORT_TIME = "hh:mm a";
+	public static final CharSequence SHORT_DATE_TIME = "EEEE, d MMM, yyy - hh:mm a";
 }
